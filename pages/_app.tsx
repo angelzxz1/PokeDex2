9c05ec6992054c56a78e6d7d0d57e300ecb58331
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2022-11-25 04:26:20 +0000

    Update 7 files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import theme from "../lib/theme";
 import { AppProps } from "next/app";
-// import Main from "../components/layouts/main";
-// import "../lib/global.css";
+import Main from "../components/layouts/main";
+import "../lib/global.css";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
 	// console.log(theme);
 	return (
 		<ChakraProvider theme={theme}>
-			{/* <Main router={router} title={Component.name}> */}
-			<Component {...pageProps} />
-			{/* </Main> */}
+			<Main router={router} title={Component.name}>
+				<Component {...pageProps} />
+			</Main>
 		</ChakraProvider>
 	);
 }
